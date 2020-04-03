cfff1d218c4ea16619fd15007657583177565934
Enrique Gomez <gomez.enrique4595@gmail.com>
2020-04-03 16:40:17 -0700

    React Redux Form Validation

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
+
 function RenderPartner({partner}) {
     if (partner) {
         return (
